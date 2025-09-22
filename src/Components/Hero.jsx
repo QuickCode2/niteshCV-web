6a6dff10cb7455df69692f51010399064faa73ef
Nitesh Ahiniya <niteshahiniya2.0@gmail.com>
2025-09-22 14:12:23 +0530

    Update Hero.jsx

--- src/Components/Hero.jsx
+++ src/Components/Hero.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { motion } from "framer-motion";
-import profileImg from "../assets/Nitesh.jpg";
+import profileImg from "../Nitesh.jpg";
 import { Link } from "react-router-dom";
 
 const Hero = () => {
