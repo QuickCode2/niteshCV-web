6056bd4c92a54e821b115c172e9c0dec4b7df96f
Nitesh Ahiniya <niteshahiniya2.0@gmail.com>
2025-09-23 11:06:43 +0530

    Update About.jsx

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,14 +1,14 @@
 import React from 'react'
-import Hero from '../Components/Hero' 
+import Hero from '../Components/Hero'
 import { motion } from 'framer-motion'
 
 const About = () => {
   return (
     <motion.div
-       initial={{ opacity: 0, y:100 }}
-       transition={{ duration: 1.5 }}
-       whileInView={{ opacity: 1, y:0 }}
-       onViewportLeave={{ once:true }}
+      initial={{ opacity: 0, y: 100 }}
+      transition={{ duration: 1.5 }}
+      whileInView={{ opacity: 1, y: 0 }}
+      viewport={{ once: true }}
       className='mb-20'
     >
       <Hero />
@@ -17,3 +17,4 @@ const About = () => {
 }
 
 export default About
+
